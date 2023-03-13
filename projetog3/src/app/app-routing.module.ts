5ee4c7ef3df5c419834a87bdf8b90b92683b6919
PaulHnsv <paulohenriquejudo98@gmail.com>
2023-03-13 19:33:25 -0300

    commit inicial frontend

--- projetog3/src/app/app-routing.module.ts
+++ projetog3/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RestaurantesComponent } from './restaurantes/restaurantes.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'Restaurantes', component: RestaurantesComponent},
+  {path: 'Login', component: LoginComponent},
+  {path: 'Admin', component: AdminComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
