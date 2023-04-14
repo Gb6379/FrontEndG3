77100187588cbac3645b1b2d12f30e0b98cf900b
Gb6379 <gabriel.am.rosa@gmail.com>
2023-04-14 14:56:40 -0300

    new service

--- projetog3/src/app/service/auth.service.ts
+++ projetog3/src/app/service/auth.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders} from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpContext, HttpHeaders, HttpResponse} from '@angular/common/http';
+import { filter, map, Observable } from 'rxjs';
 import { ApiConfigurationService } from './api-configuration.service';
 import { BaseServiceService } from './base-service.service';
+import { RegisterRequest } from '../model/register-request';
+import { StrictHttpResponse } from '../model/strict-http-response';
+import { AuthenticationResponse } from '../model/authentication-response';
+import { RequestBuilder } from './request-builder';
 
 const AUTH_API = 'http://localhost:9090/auth/';
 
@@ -15,11 +19,45 @@ const httpOptions = {
 })
 export class AuthService extends BaseServiceService {
 
-  constructor( config: ApiConfigurationService,
+  constructor( 
+    config: ApiConfigurationService,
     http: HttpClient
   ) {
     super(config, http);
   }
+
+  static readonly api_path = 'http://localhost:9090/auth/';
+
+
+   /**
+   * This method provides access to the full `HttpResponse`, allowing access to response headers.
+   * To access only the response body, use `register()` instead.
+   *
+   * This method sends `application/json` and handles request body of type `application/json`.
+   */
+    register$Response(params: {
+      body: RegisterRequest
+    },
+    context?: HttpContext
+  
+  ): Observable<StrictHttpResponse<AuthenticationResponse>> {
+  
+      const rb = new RequestBuilder(this.rootUrl, AuthService.api_path, 'post');
+      if (params) {
+        rb.body(params.body, 'application/json');
+      }
+  
+      return this.http.request(rb.build({
+        responseType: 'json',
+        accept: 'application/json',
+        context: context
+      })).pipe(
+        filter((r: any) => r instanceof HttpResponse),
+        map((r: HttpResponse<any>) => {
+          return r as StrictHttpResponse<AuthenticationResponse>;
+        })
+      );
+    }
 
   loginUser(username: string, password: string) : Observable<any> {
     return this.http.post(AUTH_API + '/user/authenticate', {
